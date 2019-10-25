19391336df58762cee3038ccbfce0efeabbdb564
Basil Shkara <bshkara@apple.com>
2019-10-25 12:52:53 -0700

    Initial Commit

---
+++ pr-test-proj/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  pr-test-proj
+//
+//  Created by Basil Shkara on 10/25/19.
+//  Copyright © 2019 Basil Shkara. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
